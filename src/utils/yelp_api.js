9ab3f4ee6b53c3a03294c75c26156176b67d44e3
Adam Garcia <gar15041@byui.edu>
2023-04-29 11:45:46 -0600

    api

--- src/utils/yelp_api.js
+++ src/utils/yelp_api.js
@@ -6,7 +6,7 @@ const Yelp = {
       method: 'GET',
       headers: {
         accept: 'application/json',
-        Authorization: `Bearer ${REACT_APP_YELP}`
+        Authorization: `Bearer ${process.env.REACT_APP_YELP}`
       }
     };
   
